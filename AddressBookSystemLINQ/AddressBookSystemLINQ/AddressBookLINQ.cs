23d3d1a03458ef650aa5acf8d7004d9521c770fb
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-21 11:48:23 +0530

    Adding Columns in Address Book Data Table

--- AddressBookSystemLINQ/AddressBookSystemLINQ/AddressBookLINQ.cs
+++ AddressBookSystemLINQ/AddressBookSystemLINQ/AddressBookLINQ.cs
@@ -10,5 +10,21 @@ namespace AddressBookSystemLINQ
         // Create Data table to store address book details
         public DataTable dataTable = new DataTable();
 
+        /// <summary>
+        /// Create DataTable 
+        /// </summary>
+        public void CreateAddressBook()
+        {
+            //create columns in dataTable
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("Zip", typeof(string));
+            dataTable.Columns.Add("PhoneNumber", typeof(string));
+            dataTable.Columns.Add("EmailID", typeof(string));
+            Console.WriteLine("Address Book Data Table Created Successfully!!");
+        }
     }
 }
